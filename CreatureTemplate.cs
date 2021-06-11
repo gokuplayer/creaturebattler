d1ca9d41e7b54c47709840e4296d6b9d2694f1de
Michael <38083360+gokuplayer@users.noreply.github.com>
2021-06-10 20:27:27 -0400

    Uploaded the two files I have made so far
    These C# files are for Unity 2019. The template is an interface and Frans is an implementation of the template, though it has an error.

---
+++ CreatureTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICreature
+{
+
+    string CreatureName { get; }
+    string CreatureType { get; }
+    float CreatureHealth { get; }
+    float CreaturePhysical { get; }
+    float CreaturePhysDef { get; }
+    float CreatureRanged { get; }
+    float CreatureRanDef { get; }
+    float CreatureSpeed { get; }
+    float CreatureMovement { get; }
+
+}
---
+++ Frans.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Frans : MonoBehaviour, ICreature
+{
+
+    public string Name = "Frans";
+    public string Type { get; set; }
+    public float Physical { get; set; }
+    public float PhysDef { get; set; }
+    public float Ranged { get; set; }
+    public float RanDef { get; set; }
+    public float Speed { get; set; }
+    public float Movement { get; set; }
+
+}
