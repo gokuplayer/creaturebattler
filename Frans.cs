c999e284e670527e91df484ebc03a518430390dc
Michael <38083360+gokuplayer@users.noreply.github.com>
2021-06-17 17:21:57 -0400

    Frans final stats
    Frans now working with the interface

--- Frans.cs
+++ Frans.cs
@@ -4,11 +4,68 @@ using UnityEngine;
 
 public class Frans : MonoBehaviour, ICreature
 {
+    public string Name;
+    public string Type;
+    public float Health;
+    public float Physical;
+    public float PhysDef;
+    public float Ranged;
+    public float RanDef;
+    public float Speed;
+    public float Movement;
 
     string ICreature.CreatureName
     {
         get { return Name; }
         set { Name = value; }
+    }
+
+    string ICreature.CreatureType
+    {
+        get { return Type; }
+        set { Type = value; }
+    }
+
+    float ICreature.CreatureHealth
+    {
+        get { return Health; }
+        set { Health = value; }
+    }
+
+    float ICreature.CreaturePhysical
+    {
+        get { return Physical; }
+        set { Physical = value; }
+    }
+
+    float ICreature.CreaturePhysDef
+    {
+        get { return PhysDef; }
+        set { PhysDef = value; }
+    }
+
+    float ICreature.CreatureRanged
+    {
+        get { return Ranged; }
+        set { Ranged = value; }
+    }
+
+    float ICreature.CreatureRanDef
+    {
+        get { return RanDef; }
+        set { RanDef = value; }
+    }
+
+    float ICreature.CreatureSpeed
+    {
+        get { return Speed; }
+        set { Speed = value; }
+    }
+
+    float ICreature.CreatureMovement
+    {
+        get { return Movement; }
+        set { Movement = value; }
     }
 
 }
