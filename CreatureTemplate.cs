c688d6599907b4a7e1fa8751fe9e27159d8d09e6
Michael <38083360+gokuplayer@users.noreply.github.com>
2021-06-17 17:12:49 -0400

    Added updated stat and template files

---
+++ CreatureTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICreature
+{
+
+    string CreatureName { get; set; }
+    string CreatureType { get; set; }
+    float CreatureHealth { get; set; }
+    float CreaturePhysical { get; set; }
+    float CreaturePhysDef { get; set; }
+    float CreatureRanged { get; set; }
+    float CreatureRanDef { get; set; }
+    float CreatureSpeed { get; set; }
+    float CreatureMovement { get; set; }
+
+}
---
+++ Frans.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frans : MonoBehaviour, ICreature
+{
+
+    string ICreature.CreatureName
+    {
+        get { return Name; }
+        set { Name = value; }
+    }
+
+}
