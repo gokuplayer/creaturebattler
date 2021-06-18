b666c905d4c298ad81d1eb1de8489e30612eb86e
Jason <jasondavis929@gmail.com>
2021-06-18 13:40:31 -0400

    change all instances of CreatureProp to just Prop for simplicity

--- ICreature.cs
+++ ICreature.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
 public interface ICreature
 {
-    string CreatureName { get; set; }
-    string CreatureType { get; set; }
-    float CreatureHealth { get; set; }
-    float CreaturePhysical { get; set; }
-    float CreaturePhysDef { get; set; }
-    float CreatureRanged { get; set; }
-    float CreatureRanDef { get; set; }
-    float CreatureSpeed { get; set; }
-    float CreatureMovement { get; set; }
+    string Name { get; set; }
+    string Type { get; set; }
+    float Health { get; set; }
+    float Physical { get; set; }
+    float PhysDef { get; set; }
+    float Ranged { get; set; }
+    float RanDef { get; set; }
+    float Speed { get; set; }
+    float Movement { get; set; }
 }
