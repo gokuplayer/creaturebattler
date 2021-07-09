8f06a2f548e11805ec2274c3faa4c295ce6749a9
gokuplayer <michaelguay13@gmail.com>
2021-07-09 02:19:16 -0400

    Starting making a way to choose custom moves for creatures. Only worked on Frans' moves

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -7,29 +7,37 @@ using UnityEngine.SceneManagement;
 public class GameControllerScript : MonoBehaviour
 {
 
-    public GameObject PlayButton, CreatureAttackerButton, Player1, Player2;
+    public GameObject PlayButton, CreatureAttackerButton, Player1, Player2, Move1Object, Move5Object;
 
-    public GameObject[] Creatures;
+    public GameObject[] Creatures, MoveArray;
 
-    public static GameObject MoveListButton, FransMovesButton, QuakeMovesButton, ReplayButton, FightButton, PlayerTextObject, PlayerChoiceTextObject, 
-        WinnerTextObject, Player1HealthTextObject, Player2HealthTextObject;
+    public static GameObject MoveListButton, Player1MovesObject, Player2MovesObject,FransMovesButton, QuakeMovesButton, ReplayButton, FightButton, 
+        ChooseMovesButton, ChooseMovesNextButton, Move1ChoiceDropdown, PlayerTextObject, PlayerChoiceTextObject, WinnerTextObject, 
+        Player1HealthTextObject, Player2HealthTextObject;
     public static Text PlayerText, PlayerCreatureChoiceText, WinnerText, Player1HealthText, Player2HealthText;
     public static int playerTurn;
     public static float player1maxHealth, player2maxHealth;
 
-    private int player1CreatureChoice, player2CreatureChoice;
+    private int player1CreatureChoice, player2CreatureChoice, move1, move5;
 
     void Start()
     {
 
+        Move1Object = GameObject.Find("Move1");
+        Move5Object = GameObject.Find("Move5");
         MoveListButton = GameObject.Find("MoveListButton");
+        Player1MovesObject = GameObject.Find("Player1Moves");
+        Player2MovesObject = GameObject.Find("Player2Moves");
         FransMovesButton = GameObject.Find("FransMoves");
         QuakeMovesButton = GameObject.Find("QuakeMoves");
         ReplayButton = GameObject.Find("ReplayButton");
         CreatureAttackerButton = GameObject.Find("Creature List");
         FightButton = GameObject.Find("FightButton");
+        ChooseMovesButton = GameObject.Find("ChooseMovesButton");
+        ChooseMovesNextButton = GameObject.Find("ChooseMovesNextButton");
         PlayerTextObject = GameObject.Find("PlayerText");
         PlayerChoiceTextObject = GameObject.Find("PlayerCreatureChoiceText");
+        Move1ChoiceDropdown = GameObject.Find("Move1ChoiceDropdown");
         WinnerTextObject = GameObject.Find("WinnerText");
         Player1HealthTextObject = GameObject.Find("Player1Health");
         Player2HealthTextObject = GameObject.Find("Player2Health");
@@ -40,12 +48,17 @@ public class GameControllerScript : MonoBehaviour
         Player2HealthText = Player2HealthTextObject.GetComponent<Text>();
 
         PlayButton.SetActive(true);
+        Player1MovesObject.SetActive(false);
+        Player2MovesObject.SetActive(false);
         FransMovesButton.SetActive(false);
         QuakeMovesButton.SetActive(false);
         MoveListButton.SetActive(false);
         ReplayButton.SetActive(false);
         CreatureAttackerButton.SetActive(false);
         FightButton.SetActive(false);
+        ChooseMovesButton.SetActive(false);
+        ChooseMovesNextButton.SetActive(false);
+        Move1ChoiceDropdown.SetActive(false);
 
         PlayerText.text = "";
         PlayerCreatureChoiceText.text = "";
@@ -131,8 +144,100 @@ public class GameControllerScript : MonoBehaviour
             player2CreatureChoice = creature;
             playerTurn = 1;
             CreatureAttackerButton.SetActive(false);
-            FightButton.SetActive(true);
+            ChooseMovesButton.SetActive(true);
             PlayerCreatureChoiceText.text = "";
+
+        }
+
+    }
+
+    public void MoveDropdownChoice(int choice)
+    {
+
+        ChooseMovesNextButton.SetActive(true);
+
+        if (playerTurn == 1)
+        {
+
+            playerTurn = 2;
+
+            switch (choice)
+            {
+
+                case 1:
+                    move1 = 1;
+                    break;
+                case 6:
+                    move1 = 2;
+                    break;
+                case 7:
+                    move1 = 0;
+                    break;
+                default:
+                    Debug.Log("Move choice error");
+                    break;
+
+            }
+
+        }
+        else if (playerTurn == 2)
+        {
+
+            playerTurn = 1;
+
+            switch (choice)
+            {
+
+                case 1:
+                    move5 = 1;
+                    break;
+                default:
+                    Debug.Log("Move choice error");
+                    break;
+
+            }
+
+        }
+
+    }
+
+    public void MoveChoice()
+    {
+
+        if (playerTurn == 1)
+        {
+
+            ChooseMovesButton.SetActive(false);
+            PlayerCreatureChoiceText.text = "Player 1, choose your moves";
+            Move1ChoiceDropdown.SetActive(true);
+
+            switch (player1CreatureChoice)
+            {
+                
+
+                default:
+                    Debug.Log("Creature choice error.");
+                    break;
+
+            }
+
+        }
+        else if (playerTurn == 2)
+        {
+
+            ChooseMovesNextButton.SetActive(false);
+            FightButton.SetActive(true);
+            playerTurn = 1;
+            PlayerCreatureChoiceText.text = "Player 2, choose your moves";
+
+            switch (player2CreatureChoice)
+            {
+
+                default:
+                    Debug.Log("Creature choice error.");
+                    break;
+
+            }
 
         }
 
@@ -142,6 +247,8 @@ public class GameControllerScript : MonoBehaviour
     {
 
         FightButton.SetActive(false);
+        Move1ChoiceDropdown.SetActive(false);
+        PlayerCreatureChoiceText.text = "";
         MoveListButton.SetActive(true);
         GameObject clone1 = Instantiate(Creatures[player1CreatureChoice], new Vector3(-1, 0, 2), Quaternion.identity);
         clone1.transform.tag = "Player1";
@@ -155,331 +262,12 @@ public class GameControllerScript : MonoBehaviour
         Creature player2Creature = Player2.GetComponent<Creature>();
         player2maxHealth = player2Creature.health;
         clone2.name = player2Creature.name + " 2";
+        GameObject moveClone1 = Instantiate(MoveArray[move1], new Vector3(0, 0, 0), Quaternion.identity);
+        moveClone1.transform.SetParent(Move1Object.transform, false);
+        GameObject moveClone5 = Instantiate(MoveArray[move5], new Vector3(0, 0, 0), Quaternion.identity);
+        moveClone5.transform.SetParent(Move5Object.transform, false);
 
     }
-
-    /*public void AngeelaAttacker()
-    {
-
-        if (playerTurn == 1)
-        {
-
-            attacker1 = "angeela";
-            playerTurn = 2;
-            PlayerCreatureChoiceText.text = "Player 2, choose your creature.";
-            GameObject clone = Instantiate(Creatures[0], new Vector3(-1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player1";
-            Player1 = GameObject.FindWithTag("Player1");
-            clone.name = "Angeela 1";
-            Creature player1Creature = Player1.GetComponent<Creature>();
-            player1maxHealth = player1Creature.health;
-
-        }
-        else if (playerTurn == 2)
-        {
-
-            attacker2 = "angeela";
-            playerTurn = 1;
-            PlayerCreatureChoiceText.text = "";
-            CreatureAttackerButton.SetActive(false);
-            MoveListButton.SetActive(true);
-            GameObject clone = Instantiate(Creatures[0], new Vector3(1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player2";
-            Player2 = GameObject.FindWithTag("Player2");
-            clone.name = "Angeela 2";
-            Creature player2Creature = Player2.GetComponent<Creature>();
-            player2maxHealth = player2Creature.health;
-
-        }
-
-    }
-
-    public void BlazzeruseAttacker()
-    {
-
-        if (playerTurn == 1)
-        {
-
-            attacker1 = "blazzeruse";
-            playerTurn = 2;
-            PlayerCreatureChoiceText.text = "Player 2, choose your creature.";
-            GameObject clone = Instantiate(Creatures[1], new Vector3(-1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player1";
-            Player1 = GameObject.FindWithTag("Player1");
-            clone.name = "Blazzeruse 1";
-            Creature player1Creature = Player1.GetComponent<Creature>();
-            player1maxHealth = player1Creature.health;
-
-        }
-        else if (playerTurn == 2)
-        {
-
-            attacker2 = "blazzeruse";
-            playerTurn = 1;
-            PlayerCreatureChoiceText.text = "";
-            CreatureAttackerButton.SetActive(false);
-            MoveListButton.SetActive(true);
-            GameObject clone = Instantiate(Creatures[1], new Vector3(1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player2";
-            Player2 = GameObject.FindWithTag("Player2");
-            clone.name = "Blazzeruse 2";
-            Creature player2Creature = Player2.GetComponent<Creature>();
-            player2maxHealth = player2Creature.health;
-
-        }
-
-    }
-
-    public void CavenardAttacker()
-    {
-
-        if (playerTurn == 1)
-        {
-
-            attacker1 = "cavenard";
-            playerTurn = 2;
-            PlayerCreatureChoiceText.text = "Player 2, choose your creature.";
-            GameObject clone = Instantiate(Creatures[2], new Vector3(-1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player1";
-            Player1 = GameObject.FindWithTag("Player1");
-            clone.name = "Cavenard 1";
-            Creature player1Creature = Player1.GetComponent<Creature>();
-            player1maxHealth = player1Creature.health;
-
-        }
-        else if (playerTurn == 2)
-        {
-
-            attacker2 = "cavenard";
-            playerTurn = 1;
-            PlayerCreatureChoiceText.text = "";
-            CreatureAttackerButton.SetActive(false);
-            MoveListButton.SetActive(true);
-            GameObject clone = Instantiate(Creatures[2], new Vector3(1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player2";
-            Player2 = GameObject.FindWithTag("Player2");
-            clone.name = "Cavenard 2";
-            Creature player2Creature = Player2.GetComponent<Creature>();
-            player2maxHealth = player2Creature.health;
-
-        }
-
-    }
-
-    public void CharrageAttacker()
-    {
-
-        if (playerTurn == 1)
-        {
-
-            attacker1 = "charrage";
-            playerTurn = 2;
-            PlayerCreatureChoiceText.text = "Player 2, choose your creature.";
-            GameObject clone = Instantiate(Creatures[3], new Vector3(-1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player1";
-            Player1 = GameObject.FindWithTag("Player1");
-            clone.name = "Angeela 1";
-            Creature player1Creature = Player1.GetComponent<Creature>();
-            player1maxHealth = player1Creature.health;
-
-        }
-        else if (playerTurn == 2)
-        {
-
-            attacker2 = "charrage";
-            playerTurn = 1;
-            PlayerCreatureChoiceText.text = "";
-            CreatureAttackerButton.SetActive(false);
-            MoveListButton.SetActive(true);
-            GameObject clone = Instantiate(Creatures[3], new Vector3(1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player2";
-            Player2 = GameObject.FindWithTag("Player2");
-            clone.name = "Charrage 2";
-            Creature player2Creature = Player2.GetComponent<Creature>();
-            player2maxHealth = player2Creature.health;
-
-        }
-
-    }
-
-    public void ContrelaAttacker()
-    {
-
-        if (playerTurn == 1)
-        {
-
-            attacker1 = "contrela";
-            playerTurn = 2;
-            PlayerCreatureChoiceText.text = "Player 2, choose your creature.";
-            GameObject clone = Instantiate(Creatures[4], new Vector3(-1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player1";
-            Player1 = GameObject.FindWithTag("Player1");
-            clone.name = "Contrela 1";
-            Creature player1Creature = Player1.GetComponent<Creature>();
-            player1maxHealth = player1Creature.health;
-
-        }
-        else if (playerTurn == 2)
-        {
-
-            attacker2 = "contrela";
-            playerTurn = 1;
-            PlayerCreatureChoiceText.text = "";
-            CreatureAttackerButton.SetActive(false);
-            MoveListButton.SetActive(true);
-            GameObject clone = Instantiate(Creatures[4], new Vector3(1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player2";
-            Player2 = GameObject.FindWithTag("Player2");
-            clone.name = "Contrela 2";
-            Creature player2Creature = Player2.GetComponent<Creature>();
-            player2maxHealth = player2Creature.health;
-
-        }
-
-    }
-
-    public void DivaneceAttacker()
-    {
-
-        if (playerTurn == 1)
-        {
-
-            attacker1 = "divanece";
-            playerTurn = 2;
-            PlayerCreatureChoiceText.text = "Player 2, choose your creature.";
-            GameObject clone = Instantiate(Creatures[5], new Vector3(-1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player1";
-            Player1 = GameObject.FindWithTag("Player1");
-            clone.name = "Divanece 1";
-            Creature player1Creature = Player1.GetComponent<Creature>();
-            player1maxHealth = player1Creature.health;
-
-        }
-        else if (playerTurn == 2)
-        {
-
-            attacker2 = "divanece";
-            playerTurn = 1;
-            PlayerCreatureChoiceText.text = "";
-            CreatureAttackerButton.SetActive(false);
-            MoveListButton.SetActive(true);
-            GameObject clone = Instantiate(Creatures[5], new Vector3(1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player2";
-            Player2 = GameObject.FindWithTag("Player2");
-            clone.name = "Divanece 2";
-            Creature player2Creature = Player2.GetComponent<Creature>();
-            player2maxHealth = player2Creature.health;
-
-        }
-
-    }
-
-    public void DrafskaAttacker()
-    {
-
-        if (playerTurn == 1)
-        {
-
-            attacker1 = "drafska";
-            playerTurn = 2;
-            PlayerCreatureChoiceText.text = "Player 2, choose your creature.";
-            GameObject clone = Instantiate(Creatures[6], new Vector3(-1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player1";
-            Player1 = GameObject.FindWithTag("Player1");
-            clone.name = "Drafska 1";
-            Creature player1Creature = Player1.GetComponent<Creature>();
-            player1maxHealth = player1Creature.health;
-
-        }
-        else if (playerTurn == 2)
-        {
-
-            attacker2 = "drafska";
-            playerTurn = 1;
-            PlayerCreatureChoiceText.text = "";
-            CreatureAttackerButton.SetActive(false);
-            MoveListButton.SetActive(true);
-            GameObject clone = Instantiate(Creatures[6], new Vector3(1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player2";
-            Player2 = GameObject.FindWithTag("Player2");
-            clone.name = "Drafska 2";
-            Creature player2Creature = Player2.GetComponent<Creature>();
-            player2maxHealth = player2Creature.health;
-
-        }
-
-    }
-
-    public void FransAttacker()
-    {
-
-        if (playerTurn == 1)
-        {
-
-            attacker1 = "frans";
-            playerTurn = 2;
-            PlayerCreatureChoiceText.text = "Player 2, choose your creature.";
-            GameObject clone = Instantiate(Creatures[10], new Vector3(-1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player1";
-            Player1 = GameObject.FindWithTag("Player1");
-            Creature player1Creature = Player1.GetComponent<Creature>();
-            player1maxHealth = player1Creature.health;
-
-        }
-        else if (playerTurn == 2)
-        {
-
-            attacker2 = "frans";
-            playerTurn = 1;
-            PlayerCreatureChoiceText.text = "";
-            CreatureAttackerButton.SetActive(false);
-            MoveListButton.SetActive(true);
-            GameObject clone = Instantiate(Creatures[10], new Vector3(1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player2";
-            Player2 = GameObject.FindWithTag("Player2");
-            clone.name = "Frans 2";
-            Creature player2Creature = Player2.GetComponent<Creature>();
-            player2maxHealth = player2Creature.health;
-
-        }
-
-    }
-
-    public void QuakeAttacker()
-    {
-
-        if (playerTurn == 1)
-        {
-
-            attacker1 = "quake";
-            playerTurn = 2;
-            PlayerCreatureChoiceText.text = "Player 2, choose your creature.";
-            GameObject clone = Instantiate(Creatures[21], new Vector3(-1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player1";
-            Player1 = GameObject.FindWithTag("Player1");
-            clone.name = "Quake 1";
-            Creature player1Creature = Player1.GetComponent<Creature>();
-            player1maxHealth = player1Creature.health;
-
-        }
-        else if (playerTurn == 2)
-        {
-
-            attacker2 = "quake";
-            playerTurn = 1;
-            PlayerCreatureChoiceText.text = "";
-            CreatureAttackerButton.SetActive(false);
-            MoveListButton.SetActive(true);
-            GameObject clone = Instantiate(Creatures[21], new Vector3(1, 0, 2), Quaternion.identity);
-            clone.transform.tag = "Player2";
-            Player2 = GameObject.FindWithTag("Player2");
-            clone.name = "Quake 2";
-            Creature player2Creature = Player2.GetComponent<Creature>();
-            player2maxHealth = player2Creature.health;
-
-        }
-
-    }*/
 
     public void Moves()
     {
@@ -489,26 +277,15 @@ public class GameControllerScript : MonoBehaviour
         if (playerTurn == 1)
         {
 
-            switch (player1CreatureChoice)
-            {
-
-                case 10:
-                    FransMovesButton.SetActive(true);
-                    break;
-                case 21:
-                    QuakeMovesButton.SetActive(true);
-                    break;
-                default:
-                    Debug.Log("Attacker not chosen. Error.");
-                    break;
-
-            }
+            Player1MovesObject.SetActive(true);
 
         }
         else if (playerTurn == 2)
         {
 
-            switch (player2CreatureChoice)
+            Player2MovesObject.SetActive(true);
+
+            /*switch (player2CreatureChoice)
             {
 
                 case 10:
@@ -521,7 +298,7 @@ public class GameControllerScript : MonoBehaviour
                     Debug.Log("Attacker not chosen. Error.");
                     break;
 
-            }
+            }*/
 
         }
 
@@ -534,23 +311,5 @@ public class GameControllerScript : MonoBehaviour
         SceneManager.LoadScene(scene.name);
 
     }
-
-    /*public void DarkStrikeAttack()
-    {
-
-        if (playerTurn == 1)
-        {
-
-            playerTurn = 2;
-
-        }
-        else if (playerTurn == 2)
-        {
-
-            playerTurn = 1;
-
-        }
-
-    }*/
 
 }
